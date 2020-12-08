da81bb02694b4cba912a1b336c665611c9e6a80f
Brenner <brenner.lattin@gmail.com>
2020-12-07 21:25:55 -0700

    Initial commit

---
+++ Item.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Item {
+    private String name;
+    private String type;
+    private double price;
+    private double weight;
+    private List<String> urls;
+}
